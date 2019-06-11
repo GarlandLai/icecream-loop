a77f63f72987f81f1ec8e092ab99110d0b79980d
Garland Lai and Nathan Wangemann <nathan@>
2019-06-11 09:47:40 -0700

    change forEach loop to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,23 @@ $(document).ready(function(){
   $("#formOne").submit(function(event) {
    //This starts the function with form one
     var kinds = ["flavor1", "flavor2", "flavor3", "flavor4", "flavor5"]
+
+    for (index = 0; index < kinds.length; index += 1) {
+      var userInput = $("#" + kinds[index]).val();
+        $('.output p').append("I like " + userInput + ".")
+    };
+        event.preventDefault();
+  });
+});
+
+
+
    //This is a variable full of the flavors of icecream
-    kinds.forEach(function(input){
-      var userInput = $("#" + input).val();
-      console.log(userInput);
-      var spaces = (", ")
-      $(".output p").append("I like " + userInput + spaces);
+    // kinds.forEach(function(input){
+    //   var userInput = $("#" + input).val();
+    //   console.log(userInput);
+    //   var spaces = (", ")
+    //   $(".output p").append("I like " + userInput + spaces);
 
 
       // $(".output").show();
-      event.preventDefault();
-    });
-  });
-});
